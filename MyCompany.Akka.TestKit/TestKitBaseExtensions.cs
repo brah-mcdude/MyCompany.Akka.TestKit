fc3fb83604bad8a7f9ee31c484462b36b88c8d19
Brah McDude <77924970+brah-mcdude@users.noreply.github.com>
2023-01-17 06:22:42 -0500

    issue repro;

--- MyCompany.Akka.TestKit/TestKitBaseExtensions.cs
+++ MyCompany.Akka.TestKit/TestKitBaseExtensions.cs
@@ -4,13 +4,13 @@ namespace MyCompany.Akka.TestKit
 {
     static public class TestKitBaseExtensions
     {
-        static public async Task ExpectLogNoWarningsNorErrorsAsync(this global::Akka.TestKit.Xunit2.TestKit x, Func<Task> actionAsync) => await x.CreateEventFilter(x.Sys)
-        .Custom(logEvent => NewMethod(logEvent))
-        .ExpectAsync(0, actionAsync, TimeSpan.FromMilliseconds(1));
+        static public async Task ExpectLogNoWarningsNorErrorsAsync(this global::Akka.TestKit.Xunit2.TestKit x, Func<Task> actionAsync) => await x.EventFilter
+            .Custom(logEvent => Predicate(logEvent))
+            .ExpectAsync(0, actionAsync, TimeSpan.FromMilliseconds(1));
 
-        private static bool NewMethod(LogEvent logEvent)
+        private static bool Predicate(LogEvent logEvent)
         {
-            bool v = logEvent is Info;
+            bool v = logEvent is Error || logEvent is Warning;
             return v;
         }
     }
