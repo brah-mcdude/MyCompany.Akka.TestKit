542d539e7f28fca78728c88350f3f904277f4af8
Brah McDude <77924970+brah-mcdude@users.noreply.github.com>
2023-01-17 06:10:06 -0500

    Add project files.

---
+++ MyCompany.Akka.TestKit/UnitTest1.cs
@@ -0,0 +1,15 @@
+using Akka.Actor;
+
+namespace MyCompany.Akka.TestKit
+{
+    public class SomeActor : ReceiveActor { }
+
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
